cac3c0cbbda0192785235fdde0a2f13dd186c461
Ross <rossycharlton@googlemail.com>
2024-01-15 01:57:56 +0000

    Added logic to display feedback messages

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,6 @@
 var highscoresOL = document.querySelector("#highscores")
 var clearButton = document.querySelector("#clear")
+var feedbackDiv = document.querySelector("#feedback")
 
 function getHighScores(){
     var highScores = JSON.parse(localStorage.getItem("scores"))
@@ -7,6 +8,7 @@ function getHighScores(){
 }
 
 function renderHighscores(highScores){
+    highscoresOL.innerHTML = ""
     if(highScores.length < 1){
         var highScoreH3 = document.createElement("h3")
         highScoreH3.textContent = `There are no highsores yet`
@@ -22,9 +24,25 @@ function renderHighscores(highScores){
 }
 
 function clearHighScores(){
-    if(getHighScores() < 1) return
+    if(getHighScores() < 1){
+        feedbackDiv.classList.remove("hide")
+        feedbackDiv.textContent = "There are currently no high scores"
+        feedbackDiv.style.color = "red"
+    
+        setTimeout(() => {
+            feedbackDiv.classList.add("hide")
+        }, 3000)
+        return
+    } 
     localStorage.setItem("scores", "[]")
     renderHighscores(getHighScores())
+    feedbackDiv.classList.remove("hide")
+    feedbackDiv.textContent = "All high scores have been reset"
+    feedbackDiv.style.color = "green"
+
+    setTimeout(() => {
+        feedbackDiv.classList.add("hide")
+    }, 3000)
 }
 
 renderHighscores(getHighScores())
